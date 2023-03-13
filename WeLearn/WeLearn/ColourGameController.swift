c38a5bc1a2ece7379dc9ea8852c12db0d6c54730
Andrei Cinca <andrei-marian.cinca@kcl.ac.uk>
2023-03-13 14:31:55 +0000

    added colour mini-game

---
+++ WeLearn/WeLearn/ColourGameController.swift
@@ -0,0 +1,35 @@
+//
+//  ColourGameController.swift
+//  WeLearn
+//
+//  Created by Andrei Cinca on 13/03/2023.
+//
+
+import UIKit
+import Foundation
+class ColourGameController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet var buttonTapped:UIButton!
+    @IBOutlet var textView:UITextView!
+    var timeElapsed = 0
+    var clicks = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the initial image
+        
+    }
+    
+    @IBAction func changeColour(_ sender: UIButton){
+        clicks=clicks+1
+        if clicks <= 10 {
+            let randomColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+            colorView.backgroundColor = randomColor
+        }
+        else{
+            textView.text="Great Job!  Now that we have recovered our focus, let us go back to studying"
+        }
+     
+    }
+}
